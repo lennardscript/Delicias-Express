55762d1f013b164f963a38d0f40fd0c3d6417682
Leandro Burgos <burgosleandro.c@gmail.com>
2024-05-15 17:14:31 -0400

    L: Database connection ready ✅

---
+++ Backend/Database/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Database
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public async Task<(bool IsConnected, string Message)> CheckDatabaseConnectionAsync()
+        {
+            try
+            {
+                // A simple query to verify the connection
+                using (var transaction = await this.Database.BeginTransactionAsync())
+                {
+                    await transaction.CommitAsync();
+                    return (true, "Database connection successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                return (false, $"Error when trying to connect to the database: {ex.Message}");
+            }
+        }
+    }
+}
